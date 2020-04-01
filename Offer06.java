f9989317905f451629d211b75972b218e097c4bb
YiKing <15567055305@163.com>
2020-04-01 17:01:23 +0800

    Offer06 debug

--- Offer06.java
+++ Offer06.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 // 面试题06. 从尾到头打印链表
 // 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
 // 示例 1：
@@ -7,8 +9,11 @@
 // 0 <= 链表长度 <= 10000
 
 
-
-
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -19,12 +24,12 @@
  */
 class Solution {
     public int[] reversePrint(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
-        while(head != null){
+        final Stack<Integer> stack = new Stack<>();
+        while (head != null) {
             stack.push(head.val);
             head = head.next;
         }
-        int [] res = new int[stack.size()];
+        final int[] res = new int[stack.size()];
         int count = 0;
         while(!stack.isEmpty()){
             res[count++] = stack.pop();
