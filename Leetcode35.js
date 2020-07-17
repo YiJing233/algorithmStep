4a05a892712ffba79b54f93550233c6f94c78f3b
YiKing <15567055305@163.com>
2020-07-17 22:42:39 +0800

    add practise 0717

---
+++ Leetcode35.js
@@ -0,0 +1,59 @@
+// 35. 搜索插入位置
+
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 你可以假设数组中无重复元素。
+
+// 示例 1:
+
+// 输入: [1,3,5,6], 5
+// 输出: 2
+
+// 示例 2:
+
+// 输入: [1,3,5,6], 2
+// 输出: 1
+
+// 示例 3:
+
+// 输入: [1,3,5,6], 7
+// 输出: 4
+
+// 示例 4:
+
+// 输入: [1,3,5,6], 0
+// 输出: 0
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ *  解法一，O(n)遍历
+ */
+var searchInsert = function (nums, target) {
+    let i = 0;
+    while (target > nums[i]) {
+        i++;
+    }
+    return i;
+};
+
+// 解法二，二分查找
+var searchInsert = function (nums, target) {
+    var left = 0;
+    var right = nums.length - 1;
+
+    while (left <= right) {
+        var mid = Math.floor((left + right) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
