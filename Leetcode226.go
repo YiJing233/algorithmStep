8d5df1eb1320d94af928288f0edadc8f9695c4d4
YiKing <15567055305@163.com>
2020-09-16 20:41:04 +0800

    add practise 0916

---
+++ Leetcode226.go
@@ -0,0 +1,48 @@
+//Invert a binary tree. 
+//
+// Example: 
+//
+// Input: 
+//
+// 
+//     4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// Output: 
+//
+// 
+//     4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// Trivia: 
+//This problem was inspired by this original tweet by Max Howell: 
+//
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you c
+//an’t invert a binary tree on a whiteboard so f*** off. 
+// Related Topics Tree 
+// 👍 3788 👎 61
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Right, root.Left = invertTree(root.Left), invertTree(root.Right)
+	return root
+}
+//leetcode submit region end(Prohibit modification and deletion)
